29fb023aefbe45e707b967bce088be0828cd18e8
MW <marian.witkowski@gmail.com>
2021-06-01 15:01:48 +0200

    next commit

--- Dzien04/alx/movies/views.py
+++ Dzien04/alx/movies/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.http.response import HttpResponse, Http404
+from .models import *
+from django.shortcuts import render, redirect, get_object_or_404
 # Create your views here.
+
+def startpage_response(request):
+    return HttpResponse("<br/>".join( dir(request) ) )
+
+def movielist_response(request):
+    all_movies = Movie.objects.all().order_by("title")
+    return render(request, "movie-list.html",
+                  context={"movies":all_movies} )
